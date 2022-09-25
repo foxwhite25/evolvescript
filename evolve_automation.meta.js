a97cf76e3ad3dd82bee296be532459b93dfc17d3
Fox_white <39846845+foxwhite25@users.noreply.github.com>
2022-09-26 06:24:02 +0800

    Update evolve_automation.meta.js

--- evolve_automation.meta.js
+++ evolve_automation.meta.js
@@ -3,13 +3,14 @@
 // @namespace    http://tampermonkey.net/
 // @version      3.3.1.107.1
 // @description  try to take over the world!
-// @downloadURL  https://github.com/by22dgb/evolvescript/raw/master/evolve_automation.user.js
-// @updateURL    https://github.com/by22dgb/evolvescript/raw/master/evolve_automation.meta.js
+// @downloadURL  https://github.com/foxwhite25/evolvescript/raw/master/evolve_automation.user.js
+// @updateURL    https://github.com/foxwhite25/evolvescript/raw/master/evolve_automation.meta.js
 // @author       Fafnir
 // @author       TMVictor
 // @author       Vollch
 // @match        https://g8hh.github.io/evolve/
 // @match        https://pmotschmann.github.io/Evolve/
+// @match        https://foxwhite25.github.io/evolve/
 // @grant        none
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
 // @require      https://code.jquery.com/ui/1.12.1/jquery-ui.min.js
