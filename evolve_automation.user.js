66b127a5ff1b4832ba4b6c6fe638a6c0214f71d3
Fox_white <39846845+foxwhite25@users.noreply.github.com>
2022-09-26 06:22:52 +0800

    Update evolve_automation.user.js

--- evolve_automation.user.js
+++ evolve_automation.user.js
@@ -10,6 +10,7 @@
 // @author       Vollch
 // @match        https://g8hh.github.io/evolve/
 // @match        https://pmotschmann.github.io/Evolve/
+// @match        https://foxwhite25.github.io/evolve/
 // @grant        none
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
 // @require      https://code.jquery.com/ui/1.12.1/jquery-ui.min.js
